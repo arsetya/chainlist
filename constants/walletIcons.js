4660c6f70352620b960f1144efea8808ac9e4b30
arsetya <arsetya432@gmail.com>
2024-12-17 12:07:25 +0700

    Update walletIcons.js

--- constants/walletIcons.js
+++ constants/walletIcons.js
@@ -1,4 +1,4 @@
-export const walletIcons = {
+export const walletIcons = {0x7ff7fa94b8b66ef313f7970d4eebd2cb3103a2c0}
   "Coinbase Wallet": "/connectors/coinbaseWalletIcon.svg",
   XDEFI: "/connectors/icn-xdefi.svg",
   Taho: "/connectors/icn-taho.svg",
